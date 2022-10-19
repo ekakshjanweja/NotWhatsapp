c1449697036f89f9e5a3c51eb5ef8b41c949c726
Ekaksh Janweja <jekaksh@gmail.com>
2022-10-19 18:23:22 +0530

    moved sender and user message card widgets to chat feature widegts

--- lib/features/chat/widgets/chat_list.dart
+++ lib/features/chat/widgets/chat_list.dart
@@ -8,8 +8,8 @@ import 'package:intl/intl.dart';
 import 'package:not_whatsapp/common/widgets/loader.dart';
 import 'package:not_whatsapp/features/chat/controller/chat_controller.dart';
 import 'package:not_whatsapp/models/message_model.dart';
-import 'package:not_whatsapp/widgets/sender_message_card.dart';
-import 'package:not_whatsapp/widgets/user_message_card.dart';
+import 'package:not_whatsapp/features/chat/widgets/sender_message_card.dart';
+import 'package:not_whatsapp/features/chat/widgets/user_message_card.dart';
 
 class ChatList extends ConsumerStatefulWidget {
   final String receiverUserId;
